438029e545628a051cb3be533485f50f8b607f8c
fnmdx111 <chsc4698@gmail.com>
2019-04-28 20:35:39 -0700

    Second commit

--- src/meta.ts
+++ src/meta.ts
@@ -1,5 +1,9 @@
 module.exports = {
     include: 'https://zasa.sakura.ne.jp/dp/*',
     require: 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.0.min.js',
-    grant: ['GM_setValue', 'GM_getValue']
+    grant: ['GM_setValue', 'GM_getValue'],
+    homepage: 'https://github.com/qwert42/denjinladder',
+    updateURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.ts',
+    downloadURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.ts',
+    supportURL: 'https://github.com/qwert42/denjinladder/issues'
 };
