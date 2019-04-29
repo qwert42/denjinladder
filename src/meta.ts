d7ac2f64962a8a6c8e0e304cb8d53a847ce0c932
fnmdx111 <chsc4698@gmail.com>
2019-04-28 20:39:46 -0700

    Fix typo

--- src/meta.ts
+++ src/meta.ts
@@ -3,7 +3,7 @@ module.exports = {
     require: 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.0.min.js',
     grant: ['GM_setValue', 'GM_getValue'],
     homepage: 'https://github.com/qwert42/denjinladder',
-    updateURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.ts',
-    downloadURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.ts',
+    updateURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.js',
+    downloadURL: 'https://github.com/qwert42/denjinladder/blob/master/dist/denjinladder.user.js',
     supportURL: 'https://github.com/qwert42/denjinladder/issues'
 };
